43cb6f05c1471b4dfdce4b8037837dcc945ffe4b
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-07-27 12:52:30 +0200

    valgrind: trace children correctly

--- media-player/cmake/Valgrind.cmake
+++ media-player/cmake/Valgrind.cmake
@@ -1,11 +1,10 @@
 # valgrind support
 
 find_program( MEMORYCHECK_COMMAND valgrind )
-set( MEMORYCHECK_COMMAND_OPTIONS "--trace-children=yes --leak-check=full" )
+set(MEMORYCHECK_COMMAND_OPTIONS "--trace-children=yes" "--leak-check=full" "--tool=memcheck" "--leak-check=yes" "--show-reachable=yes" "--num-callers=20")
 
 if(MEMORYCHECK_COMMAND)
     add_custom_target(valgrind
-        COMMAND "${MEMORYCHECK_COMMAND}" --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 ctest)
+        COMMAND "${MEMORYCHECK_COMMAND}" ${MEMORYCHECK_COMMAND_OPTIONS} ctest)
 else()
-    message(STATUS "Valgrind was not found")
 endif()
