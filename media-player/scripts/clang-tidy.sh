497e84435b1658f493bd1d11bb287942f0dd2b55
Bartosz Taczała <bartosz.taczala@mobica.com>
2017-07-31 11:09:25 +0200

    clang-tidy: use CEF_ROOT from env

--- media-player/scripts/clang-tidy.sh
+++ media-player/scripts/clang-tidy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ -z "${CEF_ROOT}" ]]; then
+    echo "Set CEF_ROOT"
+    exit 1;
+fi
+
 tmp_build_dir=$(mktemp -d)
 git_root=$(git rev-parse --show-toplevel)
 mp_dir=$git_root/media-player/
@@ -9,11 +14,11 @@ old=$(pwd)
 cd $tmp_build_dir
 
 echo "cmake $mp_dir/ -DCMAKE_BUILD_TYPE=Debug -DBUILD_WITH_EXAMPLE=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
-cmake $mp_dir/ -DCMAKE_BUILD_TYPE=Debug -DBUILD_WITH_EXAMPLE=OFF -DBUILD_WITH_UNIT_TESTS=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCEF_ROOT=/home/bata/Projects/mars-linux/cef_binaries/cef_binary_3.3071.1643.ga83b8e9_linux64 >> clang.out
+cmake $mp_dir/ -DCMAKE_BUILD_TYPE=Debug -DBUILD_WITH_EXAMPLE=OFF -DBUILD_WITH_UNIT_TESTS=OFF -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCEF_ROOT=${CEF_ROOT} >> clang.out
 cd $mp_dir
 ln -s  $tmp_build_dir/compile_commands.json
 
-/usr/share/clang/run-clang-tidy.py -header-filter=$mp_srcs_dir -checks=boost-*,clang-analyzer-*,llvm-*,performance-* -p $tmp_build_dir
+/usr/share/clang/run-clang-tidy.py -header-filter=$mp_srcs_dir -checks=boost-*,clang-analyzer-*,llvm-*,performance-* -p $tmp_build_dir -fix
 
 rm $mp_dir/compile_commands.json
 
